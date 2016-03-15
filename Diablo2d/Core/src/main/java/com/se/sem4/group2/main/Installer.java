9df77630619cf854dbc59f9c108457aabc3a185d
ThomasLemqvist <ThomasLemqvist@192.168.87.107>
2016-03-15 12:34:25 +0100

    IMPORTANT: added GUI-thread | NOT-IMPORTANT: Added GNU GPL 3.0 License to all important files

--- Diablo2d/Core/src/main/java/com/se/sem4/group2/main/Installer.java
+++ Diablo2d/Core/src/main/java/com/se/sem4/group2/main/Installer.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2016 Casper Beese Nielsen, Jakob Tøttrup, Jesper Bager Rasmussen, Oliver Vestergaard, Simon Hjortshøj Larsen & Thomas August Lemqvist
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.se.sem4.group2.main;
 
@@ -13,23 +24,27 @@ import org.openide.modules.ModuleInstall;
  *
  * @author ThomasLemqvist
  */
-
 public class Installer extends ModuleInstall {
-    
+
     @Override
-    public void restored(){
-        //TODO: tråd
-//        Game game = new Game();
-    LwjglApplicationConfiguration cfg =
-			new LwjglApplicationConfiguration();
-		cfg.title = "Diablo ii-D";
-		cfg.width = 640;
-		cfg.height = 480;
-		cfg.useGL30 = false;
-		cfg.resizable = false;
-		
-		new LwjglApplication(new Game(), cfg);
-            
+    public void restored() {
+        StarterThread t1 = new StarterThread();
+        t1.run();
     }
-    
+}
+
+class StarterThread extends Thread {
+
+    public void run() {
+        LwjglApplicationConfiguration cfg
+                = new LwjglApplicationConfiguration();
+        cfg.title = "Diablo ii-D";
+        cfg.width = 640;
+        cfg.height = 480;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(new Game(), cfg);
+    }
+
 }
