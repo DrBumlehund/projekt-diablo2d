ed3e8f0ae35f488793a82ccf1641d24a7d710f84
Thomas August Lemqvist <dr.bumlehund@live.dk>
2016-03-17 22:54:49 +0100

    Projectile v2.0 ¯\_(ツ)_/¯ git revert reimplementation #builder

--- Diablo2d/Projectile/src/main/java/com/se/sem4/group2/projectile/ProjectileProcessor.java
+++ Diablo2d/Projectile/src/main/java/com/se/sem4/group2/projectile/ProjectileProcessor.java
@@ -5,10 +5,47 @@
  */
 package com.se.sem4.group2.projectile;
 
+import com.se.sem4.group2.common.data.Entity;
+import com.se.sem4.group2.common.data.EntityType;
+import com.se.sem4.group2.common.data.MetaData;
+import com.se.sem4.group2.common.services.IEntityProcessingService;
+import java.util.Map;
+import org.openide.util.lookup.ServiceProvider;
+
 /**
  *
  * @author Thomas
  */
-public class ProjectileProcessor {
-    
+@ServiceProvider(service = com.se.sem4.group2.common.services.IEntityProcessingService.class)
+public class ProjectileProcessor implements IEntityProcessingService {
+
+    @Override
+    public void process(MetaData metaData, Map<String, Entity> world, Entity entity) {
+        if (entity.getType() == EntityType.PROJECTILE) {
+
+            float x = entity.getX();
+            float y = entity.getY();
+            float dt = metaData.getDelta();
+            float dx = entity.getDx();
+            float dy = entity.getDy();
+
+            x = dx * dt;
+            if (x > metaData.getDisplayWidth()) {
+                x = 0;
+            } else if (x < 0) {
+                x = metaData.getDisplayWidth();
+            }
+
+            y = dy * dt;
+            if (y > metaData.getDisplayHeight()) {
+                y = 0;
+            } else if (y < 0) {
+                y = metaData.getDisplayHeight();
+            }
+
+            entity.setPos(x, y);
+
+        }
+
+    }
 }
